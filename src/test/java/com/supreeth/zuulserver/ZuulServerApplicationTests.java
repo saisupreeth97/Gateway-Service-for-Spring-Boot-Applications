4c8c89f0ed31af74f1886b0d895765fbf9be8e91
supreeth <supreeth@supreeths-MBP.lan>
2024-04-06 08:43:01 -0700

    Gateway service for Spring Boot Applications

---
+++ src/test/java/com/supreeth/zuulserver/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supreeth.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
